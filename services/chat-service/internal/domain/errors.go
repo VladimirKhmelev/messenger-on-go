a7638dda3cfec21d8fb73c9d7238bd49dacc001f
Khmelev Vladimir <khmelevvladimir2006@gmail.com>
2026-07-28 14:13:41 +0300

    chat: add auth-service gRPC client for user validate

--- services/chat-service/internal/domain/errors.go
+++ services/chat-service/internal/domain/errors.go
@@ -3,8 +3,10 @@ package domain
 import "errors"
 
 var (
-	ErrChatNotFound      = errors.New("chat not found")
-	ErrNotChatMember     = errors.New("user is not a member of this chat")
-	ErrEmptyMessage      = errors.New("message body must not be empty")
-	ErrChatAlreadyExists = errors.New("private chat between these users already exists")
+	ErrChatNotFound       = errors.New("chat not found")
+	ErrNotChatMember      = errors.New("user is not a member of this chat")
+	ErrEmptyMessage       = errors.New("message body must not be empty")
+	ErrChatAlreadyExists  = errors.New("private chat between these users already exists")
+	ErrCannotChatWithSelf = errors.New("cannot create a chat with yourself")
+	ErrTargetUserNotFound = errors.New("target user not found")
 )
